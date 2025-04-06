bed9f7990c8354ce433b97aa698beceabd039e7d
ask <xue65535@gmail.com>
2025-04-06 10:09:35 +0800

    config bug

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,8 +6,17 @@
 import { registerAs } from '@nestjs/config';
 import { AmLogLevel } from '@prisma/client';
 
+const LogLevel = {
+  DEBUG: 'debug',
+  INFO: 'info',
+  WARN: 'warn',
+  ERROR: 'error',
+  FATAL: 'fatal',
+} as const;
+
 export default registerAs('app', () => ({
   level: process.env.LOG_LEVEL || AmLogLevel.INFO, // warn
+  // level: process.env.LOG_LEVEL || LogLevel.INFO, // debug
   // level: process.env.LOG_LEVEL ,   //  debug
   // level: process.env.LOG_LEVEL ?? 'info' ,   //  debug
 }));
